07e7a5692abe9f761b893804ea25cab40e2d6072
protosto <48259030+protosto@users.noreply.github.com>
2025-11-17 08:42:49 +0100

    Add files via upload

---
+++ L2/z3/output.cpp
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+#include <not//comment>
+#include "not//comment"
+
+
+
+
+
+
+
+
+#include <iostream>
+
+
+
+
+ 
+int q;
+
+
+void nuke(void);
+
+
+#define TEST 1
+
+
+intf(){return 42;}
+
+
+int main(void) {
+    
+    std::cout << TEST << "\n";
+
+    printf("/* foo bar");
+    
+
+    
+    std::cout << "/* is great */"
+        << " and so is //"\
+    "\n";
+
+    
+    std::cout << ":o"
+    
+    "a\n";
+
+    
+    std::cout << f() << "\n";
+}
+
